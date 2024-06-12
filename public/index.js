8b1ea9d49c66e385d8b06253c80e82dd90e2234a
MarkusRaudbergetj <raudbergetj@gmail.com>
2024-06-12 21:10:51 +0200

    added structure to html file and added placeholder texture and css file

--- public/index.js
+++ public/index.js
@@ -1,23 +1,16 @@
 window.onload = function() {
-    setInterval(function() {
-        displayFile();
-    }, 100);
+    initBoard();
 };
 
 async function writeFile(text2write) {
-    try {
-        var response = await fetch("/write", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ text: text2write })
-        });
-        const result = await response.text();
-        console.log("Wrote: ", result);
-    } catch(error) {
-        console.error(error);
-    }
+    var response = await fetch("/write", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ text: text2write })
+    });
+    const result = await response.text();
 }
 
 async function readFile() {
@@ -27,24 +20,14 @@ async function readFile() {
 }
 
 async function displayFile() {
-    console.log("Started");
-    var display = document.getElementById("display");
-    const response = await fetch("/read");
-    const content = await response.text();
-    display.innerHTML = content;
-    console.log(display.innerHTML + " " + content);
+    // ...
 }
 
-async function add1() {
-    console.log("Calling add1");
-    var text = await readFile();
-    var number = parseInt(text);
-    if(isNaN(number)) {
-        number = 0;
-    } else {
-        number++;
+function initBoard() {
+    var board = document.getElementById("bgrid");
+    for(var i = 0; i < 6; i++) {
+        for(var j = 0; j < 7; j++) {
+            board.innerHTML += "<img class=\"board\" src=\"textures/placeholder.jfif\" alt=\"what\">";
+        }
     }
-
-    await writeFile(number.toString());
-    await displayFile();
 }
